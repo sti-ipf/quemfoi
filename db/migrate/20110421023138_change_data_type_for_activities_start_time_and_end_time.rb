5d383c7335678fbcba5e8f879f46034115bffc65
Fabrício Ferrari de Campos <ffc.fabricio@gmail.com>
2011-04-29 11:10:23 -0300

    uncommented alter table in migration 'change_data_type_for_activities_start_time_and_end_time' and rspec task - modifications for heroku

--- db/migrate/20110421023138_change_data_type_for_activities_start_time_and_end_time.rb
+++ db/migrate/20110421023138_change_data_type_for_activities_start_time_and_end_time.rb
@@ -1,13 +1,13 @@
 class ChangeDataTypeForActivitiesStartTimeAndEndTime < ActiveRecord::Migration
   def self.up
-#    change_table :activities do |t|
-#      t.change :start_time, :datetime
-#      t.change :end_time, :datetime
-#    end
-  execute("ALTER TABLE activities DROP COLUMN start_time;")
-  execute("ALTER TABLE activities ADD COLUMN start_time timestamp;")
-  execute("ALTER TABLE activities DROP COLUMN end_time;")
-  execute("ALTER TABLE activities ADD COLUMN end_time timestamp;")
+    change_table :activities do |t|
+      t.change :start_time, :datetime
+      t.change :end_time, :datetime
+    end
+#  execute("ALTER TABLE activities DROP COLUMN start_time;")
+#  execute("ALTER TABLE activities ADD COLUMN start_time timestamp;")
+#  execute("ALTER TABLE activities DROP COLUMN end_time;")
+#  execute("ALTER TABLE activities ADD COLUMN end_time timestamp;")
   end
 
   def self.down
