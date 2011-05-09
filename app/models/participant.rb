9858f94180fbf9410ee864a08048a5dfd63a2d2e
Fabrício Ferrari de Campos <ffc.fabricio@gmail.com>
2011-05-09 11:04:14 -0300

    added method to update incorrect participants

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -8,5 +8,26 @@ class Participant < ActiveRecord::Base
     Participant.all(:select => 'name', :group => 'name').collect {|p| p.name.gsub("'","")}
   end
 
+  def self.update_incorrect_participants(params={})
+    name_condition = ''
+    selected_names = params[:selected_names]
+    course_id = params[:course_id].to_i
+    correct_name = params[:correct_name]
+    selected_names.each do |name|
+      name_condition << "name = '#{name}'"
+      name_condition << " OR " if name != selected_names.last
+    end
+    incorrect_participants = Participant.find_by_sql("
+      SELECT * FROM participants p
+      WHERE course_id = #{course_id}
+      AND (#{name_condition})
+    ")
+    correct_participant = Participant.first(:conditions => "course_id = #{course_id} AND name = '#{correct_name}'")
+    incorrect_participants.each do |p|
+      ActiveRecord::Base.connection.execute("UPDATE activities_participants set participant_id = #{correct_participant.id} WHERE participant_id = #{p.id}")
+      p.destroy
+    end
+  end
+
 end
 
