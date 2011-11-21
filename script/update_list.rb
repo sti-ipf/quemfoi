dd8477f73baa371535f5c24d2a47bfb4e00e060e
Fabrício Ferrari de Campos <ffc.fabricio@gmail.com>
2011-11-21 15:21:24 -0200

    adicionado script que atualiza a list do participante

--- script/update_list.rb
+++ script/update_list.rb
@@ -6,8 +6,8 @@ require File.dirname(__FILE__)+'/../config/environment'
 Participant.all.each do |p|
   formation_numbers = []
   p.activities.collect(&:identificator_number).each do |f|
-    formation_numbers << f if !f.blank?
+    formation_numbers << f if !f.blank? and !formation_numbers.include?(f)
   end
-  p.list = formation_numbers.join(", ")
+  p.list = formation_numbers.join(" ")
   p.save
 end
