f9055240bb4c8eb992a269e30a638b23d3eaa6a4
Fabrício Ferrari de Campos <ffc.fabricio@gmail.com>
2011-11-21 14:34:11 -0200

    adicionado script que atualiza a list do participante

---
+++ script/update_list.rb
@@ -0,0 +1,13 @@
+# -*- encoding : utf-8 -*-
+require 'rubygems'
+require File.dirname(__FILE__)+'/../config/application'
+require File.dirname(__FILE__)+'/../config/environment'
+
+Participant.all.each do |p|
+  formation_numbers = []
+  p.activities.collect(&:formation).each do |f|
+    formation_numbers << f if !f.blank?
+  end
+  p.list = formation_numbers.join(", ")
+  p.save
+end
