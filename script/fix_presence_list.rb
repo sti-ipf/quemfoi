2e53bac8197cce5fa34db1d95181e0f5a080ec9e
Fabrício Ferrari de Campos <ffc.fabricio@gmail.com>
2011-12-12 19:40:53 -0200

    arrumado script que atualiza a lista de presenca

--- script/fix_presence_list.rb
+++ script/fix_presence_list.rb
@@ -1,23 +1,17 @@
-course = Course.first
-
-
-course.activities.each do |a|
-  participant_ids = ActivitiesParticipant.all(:select => :participant_id).collect(&:participant_id).join(',')
-  if !participant_ids.blank?
-    participants = Participant.all(:conditions => "course_id != #{course.id} AND id IN (#{participant_ids})")
-    participants.each do |p|
-      p.course_id = course.id
-      p.save
-      _participants = Participant.all(:conditions => "course_id = #{course.id} AND name = \"#{p.name}\"")
-      if _participants.count > 1
-        first = _participants.first
-        _participants.each do |pp|
-        ActiveRecord::Base.connection.execute("UPDATE activities_participants SET participant_id = #{first.id} WHERE participant_id = #{pp.id}")
-        if pp.id != first.id
-          pp.destroy
-        end
+Course.all.each do |course|
+  course.activities.each do |a|
+    participant_ids = ActivitiesParticipant.all(:select => :participant_id, :conditions => "activity_id = #{a.id}").collect(&:participant_id).join(',')
+    if !participant_ids.blank?
+      participants = Participant.all(:conditions => "course_id != #{course.id} AND id IN (#{participant_ids})")
+      participants.each do |p|
+        participant = Participant.first(:conditions => "course_id = #{course.id} AND name = \"#{p.name}\"")
+        if !participant.nil?
+          ActiveRecord::Base.connection.execute("UPDATE activities_participants SET participant_id = #{participant.id} WHERE participant_id = #{p.id} AND activity_id = #{a.id}")
+        else
+          participant = Participant.create(:name => p.name, :group => p.group, :unit => p.unit, :contact => p.contact, :course_id => course.id, :list => p.list)
+          ActiveRecord::Base.connection.execute("UPDATE activities_participants SET participant_id = #{participant.id} WHERE participant_id = #{p.id} AND activity_id = #{a.id}")
+        end  
       end
-    end
     end
   end
 end
