5ae6d7d5e02ef77d006156f829f2a71d743e806f
Fabrício Ferrari de Campos <ffc.fabricio@gmail.com>
2011-12-13 18:43:07 -0200

    atualizado script que atualiza as atividades

--- script/update_activities.rb
+++ script/update_activities.rb
@@ -1,21 +1,34 @@
 
-FasterCSV.foreach('tmp/atividades.csv') do |row|
-  activities = Activity.all(:conditions => "course_id = 34")
-  activity = nil
-  number = nil
-  date = nil
-  activities.each do |a|
-    name = a.name.split(' ').first.to_i
-    n = row.last.to_i
+# FasterCSV.foreach('tmp/atividades.csv') do |row|
+#   activities = Activity.all(:conditions => "course_id = 34")
+#   activity = nil
+#   number = nil
+#   date = nil
+#   activities.each do |a|
+#     name = a.name.split(' ').first.to_i
+#     n = row.last.to_i
  
-    if name == n || a.identificator_number.try(:include?,row.last)
-      activity = a 
-      number = a.identificator_number
-      date = Date.strptime("#{row[1]}", "%m/%d/%y")
-    end
-    break if !activity.nil?
+#     if name == n || a.identificator_number.try(:include?,row.last)
+#       activity = a 
+#       number = a.identificator_number
+#       date = Date.strptime("#{row[1]}", "%m/%d/%y")
+#     end
+#     break if !activity.nil?
     
-  end
+#   end
   
-  activity.update_attributes(:name => row[0], :date => date, :place => row[2], :identificator_number => row.last) if !activity.nil?
+#   activity.update_attributes(:name => row[0], :date => date, :place => row[2], :identificator_number => row.last) if !activity.nil?
+# end
+
+
+FasterCSV.foreach('tmp/atividades.csv') do |row|
+  id = row[0]
+  name = row[1]
+  date = Date.strptime("#{row[2]}", "%m/%d/%y")
+  place = row[3]
+  ni = row[4]
+  activity = Activity.first(:conditions => "id = #{id}")
+  if !activity.nil?
+    activity.update_attributes(:name => name, :date => date, :place => place, :identificator_number => ni)
+  end
 end
