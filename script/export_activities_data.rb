5f754d3f618482f94d14839ece4c775177590846
Fabrício Ferrari de Campos <ffc.fabricio@gmail.com>
2012-01-10 16:49:02 -0200

    trabalhando nas ultimas mudancas

--- script/export_activities_data.rb
+++ script/export_activities_data.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 
-activities = Activitiy.find_by_sql("
+activities = Activity.find_by_sql("
   SELECT c.`reference_code`, c.identifier, a.name, a.`identificator_number`, a.`date`, a.start_time, a.end_time, a.place, a.leader 
   FROM activities a
   INNER JOIN courses c where a.course_id = c.id
@@ -9,8 +9,8 @@ file_name = 'atividades'
 data = []
 activities.each do |a|
   data << [
-    a.reference_code, a.identifier, a.name, a.identificator_number, a.date.strftime("%d/%M/%Y"), 
-    a.start_time.strftime("%d/%M/%Y"),  a.end_time.strftime("%d/%M/%Y"), a.place, a.leader
+    a.reference_code, a.identifier, a.name, a.identificator_number, a.date.strftime("%d/%m/%Y"), 
+    a.start_time.strftime("%d/%m/%Y"),  a.end_time.strftime("%d/%m/%Y"), a.place, a.leader
     ]
 
 
